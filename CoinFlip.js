34b06b97273ed57b34671f57f954f4e004e60896
adeankush3 <adeankush3@gmail.com>
2022-03-31 15:40:17 +0530

    [JS All Programming COnstructor Complete]

---
+++ CoinFlip.js
@@ -0,0 +1,10 @@
+let flip = Math.floor(Math.random()*10)%2;
+
+if(flip == 1)
+{
+    console.log("Heads");
+}
+else
+{
+    console.log("Tails");
+}
---
+++ LeapYear.js
@@ -0,0 +1,18 @@
+const inputYear = 2000;
+
+if(inputYear % 400==0)
+{
+    console.log("leap year");
+}
+else if(inputYear % 100 == 0)
+{
+    console.log("Not leap year");
+}
+else if(inputYear % 4 ==0)
+{
+    console.log("leap year");
+}
+else
+{
+    console.log("Not leap year");
+}
